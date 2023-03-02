ef5726f41710dc7f7ff9a290e246d1f6f4b418bc
quangphan <quangphan1607@gmail.com>
2023-03-02 14:05:04 +0700

    modify main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
+screen.title("Welcome to Snake game")
 screen.bgcolor("black")
 screen.tracer(0)
 
@@ -15,14 +16,10 @@ screen.onkey(snake.down, "Down")
 screen.onkey(snake.right, "Right")
 screen.onkey(snake.left, "Left")
 
-
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
-
-
-
 
 screen.exitonclick()
